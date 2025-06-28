e2dd48f618675492162a5226194c5f71deee6081
Davie-N <anytka-97@mail.ru>
2025-06-28 21:54:46 +0500

    Добавлена запись экрана

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/N-Davie/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/N-Davie/python-project-49/actions)
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=N-Davie_python-project-49&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=N-Davie_python-project-49)
 [Asciinema](https://asciinema.org/a/kmH6Zjy5HlpEMI5i7vAzaWkv2)
+<asciinema-player src="https://asciinema.org/a/MwpAqmr8EPR5qpvmq9KquMMKG.js" id="mycast"></asciinema-player>
+<script src="https://asciinema.org/a/MwpAqmr8EPR5qpvmq9KquMMKG.js" id="asciicast"></script>
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,36 +1,37 @@
 import random
+import operator
+
+def get_question_and_answer():
+    operations = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+    }
+    num1 = random.randint(1, 50)
+    num2 = random.randint(1, 50)
+    op_symbol, operation = random.choice(list(operations.items()))
+    question = f"{num1} {op_symbol} {num2}"
+    answer = str(operation(num1, num2))
+    return question, answer
 
 def main():
-    print('Welcome to the Brain Games!')
-    name = input('May I have your name? ')
-    print(f'Hello, {name}!')
-    print('What is the result of the expression?')
+    print("Welcome to the Brain Games!")
+    global NAME
+    NAME = input("May I have your name? ")
+    print(f"Hello, {NAME}!")
+    print("What is the result of the expression?")
 
     for _ in range(3):
-        number_1 = random.randint(1, 100)
-        number_2 = random.randint(1, 100)
-
-        operations = ['+', '-', '*']
-        operation = random.choice(operations)
-
-        print(f'Question: {number_1} {operation} {number_2}')
-        answer = input('Your answer: ')
-
-        if operation == '+':
-            correct_answer = number_1 + number_2
-        elif operation == '-':
-            correct_answer = number_1 - number_2
+        question, correct_answer = get_question_and_answer()
+        print(f"Question: {question}")
+        user_answer = input("Your answer: ")
+        if user_answer == correct_answer:
+            print("Correct!")
         else:
-            correct_answer = number_1 * number_2
-
-        if int(answer) == correct_answer:
-            print('Correct!')
-        else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {NAME}!")
             return
-    print(f"Congratulations, {name}!")
+    print(f"Congratulations, {NAME}!")
 
 if __name__ == "__main__":
     main()
-
