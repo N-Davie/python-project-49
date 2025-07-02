2263be316b0c41013815f5187b8e77eb21de10ea
Davie <anytka-97@mail.ru>
2025-07-02 07:45:15 +0500

    Добавлен Nosonar

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -15,8 +15,8 @@ def main():
 
     number_of_questions = 3
     for _ in range(number_of_questions):
-        num1 = random.randint(1, 100)
-        num2 = random.randint(1, 100)
+        num1 = random.randint(1, 100) #NOSONAR
+        num2 = random.randint(1, 100) #NOSONAR
         correct_answer = str(gcd(num1, num2))
         print(f"Question: {num1} {num2}")
         user_answer = input("Your answer: ")
